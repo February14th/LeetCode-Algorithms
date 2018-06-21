8943610f0f839567214678c77c802805a1554037
February14th <33635405+February14th@users.noreply.github.com>
2018-06-21 17:36:33 -0400

    Create reverseString.cpp

---
+++ reverseString.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    string reverseString(string s) {
+      string result = "";
+
+        for(int i = s.length() - 1; i >= 0; i--){
+            result.push_back(s[i]);
+        }
+
+        return result;  
+    }
+};
